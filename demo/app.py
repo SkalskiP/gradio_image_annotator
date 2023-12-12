2a0caa186d759496502f6f3f2703424a8ff4dc93
SkalskiP <piotr.skalski92@gmail.com>
2023-12-12 14:06:46 +0100

    save

--- demo/app.py
+++ demo/app.py
@@ -5,12 +5,15 @@ from gradio_image_annotator import ImageAnnotator
 
 example = ImageAnnotator().example_inputs()
 
-demo = gr.Interface(
-    lambda x:x,
-    ImageAnnotator(),  # interactive version of your component
-    ImageAnnotator(),  # static version of your component
-    # examples=[[example]],  # uncomment this line to view the "example version" of your component
-)
+# demo = gr.Interface(
+#     lambda x:x,
+#     ImageAnnotator(),  # interactive version of your component
+#     ImageAnnotator(),  # static version of your component
+# )
 
+with gr.Blocks() as demo:
+    with gr.Row():
+        gr.ImageEditor()
+        ImageAnnotator()
 
 demo.launch()
